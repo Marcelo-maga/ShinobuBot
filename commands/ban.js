141c27038e559cab2854285600fdcc554344b97c
Marcelo Magalhães <marcelo.magalhaes13@etec.sp.gov.br>
2020-08-12 15:20:58 -0300

    Alterção de código para semelhança com o comando Ban

--- commands/ban.js
+++ commands/ban.js
@@ -2,11 +2,11 @@ const Discord = require('discord.js');
 const { prefix } = require('../config.json');
 
 module.exports.run = async (client, message, args) => {
-    if (message.content.startsWith(`${prefix}kick`)) {
+    if (message.content.startsWith(`${prefix}ban`)) {
 
         let member = message.mentions.members.first();
-        member.kick().then((member) => {
-            message.channel.send(`Chutei ele com toda minha força!`);
+        member.ban().then((member) => {
+            message.channel.send(`Espero nunca mais ver ele aqui`);
         }).catch(() => {
             if (!message.member.hasPermission(['BAN_MEMBERS', 'ADMINISTRATOR'])) {
                 message.reply("Não estou vendo seu cargo!");
