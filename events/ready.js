bee7a5f023d41952bb818ec3b64956956df429f7
Marcelo Magalhães <marcelo.magalhaes13@etec.sp.gov.br>
2020-08-12 16:34:25 -0300

    Status do bot ao iniciar

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,22 @@
 const Discord = require('discord.js');
-
+const { prefix } = require('../config.json');
 module.exports = async (client, message, args) => {
-    console.log(`Estou Pronto!`)
-    console.log(`Conectado como ${client.user.tag}!`)
-} 
+        console.log(`Estou Pronto!`);
+        console.log(`Conectado como ${client.user.tag}!`);
+
+        let activities = [
+            `o Araragi morrer`,
+            `os donuts fritarem`,
+            `os peitos da Hanekawa balançarem`,
+            `Peça ajuda em ${prefix}help`,
+            `Meu programador é vagabundo!`,
+            `Mande um beijo em ${prefix}kiss`
+        ],
+            i = 0;
+        setInterval(() => client.user.setActivity(`${activities[i++ %
+            activities.length]}`, {
+            type: "WATCHING"
+        }), 1000 * 60);
+        client.user
+            .setStatus("online");
+    };
