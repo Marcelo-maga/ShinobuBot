3f05976510974261192e72594a046997975dd225
Marcelo Magalhães <marcelo.magalhaes13@etec.sp.gov.br>
2020-08-12 22:30:53 -0300

    Comando Fatos

--- commands/help.js
+++ commands/help.js
@@ -9,7 +9,7 @@ exports.run = async (client, message, args) => {
       .setDescription(`${message.author}, fique tranquilo!`)
       .setColor("#FFFF00")
       .addFields(
-        {name:`${prefix}avatar`, value:`Envia seu avatar no chat :child:`},
+        {name:`${prefix}avatar`, value:`Envia seu lindo avatar no chat :child:`},
         {name:`${prefix}c`, value:`Apaga suas mensagens indevidas :roll_of_paper:`},
         {name:`${prefix}coinflip`, value:`Joga Cara ou Coroa :candy:`},
         {name:`${prefix}cor`, value:`Muda a cor do seu nome :yellow_circle:`},
@@ -17,7 +17,8 @@ exports.run = async (client, message, args) => {
         {name:`${prefix}repo`, value:`Envia meu repositorio no GitHub :keyboard:`},
         {name:`${prefix}say`, value:`Falo oque você quiser :joy:`},
         {name:`${prefix}kick`, value:`Vamos chutar todos eles!`},
-        {name: `${prefix}ban`, value:`Banimento rápido e fácil`}
+        {name: `${prefix}ban`, value:`Banimento rápido e fácil`},
+        {name:`${prefix}fatos`, value:`Cuspo fatos na sua cara`}
       )
       helpEmbed.setTimestamp();
 
